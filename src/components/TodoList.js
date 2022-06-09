55914c99321289ae6c164e3748b8fb2d17025d80
literate <gaekoon@gmail.com>
2022-06-10 01:22:20 +0900

    디테일 마크업 설정

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,16 +1,25 @@
 import React from "react";
 import styled from "styled-components";
+import TodoItem from "./TodoItem";
 
 const TodoListBlock = styled.div`
   flex: 1;
   padding: 20px 32px;
   padding-bottom: 48px;
   overflow-y: auto;
-  background: gray;
 `;
 
 function TodoList() {
-  return <TodoListBlock>To do list</TodoListBlock>;
+  return (
+    <TodoListBlock>
+      <TodoItem text="프로젝트 생성하기" done={true} />
+      <TodoItem text="프로젝트 생성하기2" done={true} />
+      <TodoItem text="프로젝트 생성하기3" done={true} />
+      <TodoItem text="프로젝트 생성하기4" done={false} />
+      <TodoItem text="프로젝트 생성하기5" done={true} />
+      <TodoItem text="프로젝트 생성하기6" done={false} />
+    </TodoListBlock>
+  );
 }
 
 export default TodoList;
